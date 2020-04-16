0c38ff2576f0be8f5b7fd838532866da8ab1699f
interns@amaris.ai <sleepyboy123@hotmail.com>
2020-04-16 20:49:54 +1000

    ReactJS First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from '@material-ui/core';
+import Cards from './components/Cards/Cards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './App.module.css';
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            seconds: 60,
+            status: true
+        };
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+      }
+
+    countDown() {
+        let reducedTime = this.state.seconds - 1;
+        this.setState({
+            seconds: reducedTime
+        })
+        if (reducedTime === -1) {
+            alert("Time's Up!");
+            this.setState({
+                status: true,
+                seconds: 60
+            })
+            clearInterval(this.timer);
+            this.timer = 0;
+        }
+    }
+
+    startTimer() {
+        if (this.timer === 0 && this.state.seconds > 0) {
+            this.setState({
+                status: false
+            })
+            this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Typography variant="h3" className={styles.title}>SleepyTaboo</Typography>
+                <div className={styles.titleBox}>
+                {(this.state.status) ?
+                <Typography variant="h4" className={styles.title} onClick={this.startTimer}>Start</Typography> 
+                :
+                <Typography variant="h4" className={styles.title}>{this.state.seconds}</Typography>
+                }
+                </div>
+                <Cards />
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,53 @@
+import React , {useState} from 'react';
+import { Card, Typography } from '@material-ui/core';
+
+import styles from './Cards.module.css';
+
+let completed = [];
+
+const Cards = () => {
+
+    let data = require('../../data.json');
+    let i = Math.round(Math.random() * ((data.length - 1) - 0));
+    const [random, setRandom] = useState(i);
+
+    function skipButton() {
+        if (completed.length === data.length) {
+            return;
+        }
+        let a = Math.round(Math.random() * ((data.length - 1) - 0));
+        while (a === random) {
+            a = Math.round(Math.random() * ((data.length - 1) - 0));
+        }
+        setRandom(a);
+    }
+
+    function nextButton() {
+        completed.push(random);
+        if (completed.length === data.length) {
+            return;
+        }
+        let a = Math.round(Math.random() * ((data.length - 1) - 0));
+        while (completed.includes(a)) {
+            a = Math.round(Math.random() * ((data.length - 1) - 0));
+        }
+        setRandom(a);
+    }
+
+    return (
+        <Card className={styles.container}>
+            <Typography variant="h4" className={styles.keyWord}>
+                {data[random].keyWord}
+            </Typography>
+            {data[random].badWord.map((word) => <Typography variant="h5" className={styles.badWord}>{word}</Typography>)}
+            <div className={styles.buttonContainer}>
+                <img className={styles.button} src="https://img.icons8.com/flat_round/48/000000/delete-sign.png" alt="cross" onClick={() => skipButton()}/>
+            </div>
+            <div className={styles.buttonContainer}>
+                <img className={styles.button} src="https://img.icons8.com/officel/48/000000/checked.png" alt="tick" onClick={() => nextButton()}/>
+            </div>
+        </Card>
+    )
+}
+
+export default Cards;
